99cae78fd2f5551fe90960abb5cf370a09429265
Gabriel <gabrielrwaing@gmail.com>
2026-04-30 04:32:04 +0800

    update

---
+++ Hubs/NotificationHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace MakauTech.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        // Send message to ALL connected clients
+        public async Task SendNotification(string message)
+        {
+            await Clients.All.SendAsync("ReceiveNotification", message);
+        }
+
+        // Optional: send to specific user
+        public async Task SendToUser(string user, string message)
+        {
+            await Clients.User(user).SendAsync("ReceiveNotification", message);
+        }
+    }
+}
